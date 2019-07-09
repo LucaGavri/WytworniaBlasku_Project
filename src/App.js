11a509efb5938dbffac955188ac40a5421fd4c86
Lukasz Gawrys <lukaszgawrys13@gmail.com>
2019-07-10 01:49:14 +0200

    folders, stock line and slider bg

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './App.css';
 import './App.scss';
 import Header from './header/_header.js'
+import Marquee from "./marquee/_marquee.js";
+import Slider from "./slider/_slider.js";
 
 function App() {
-  return (
-    <Header/>
-  );
+    return (
+        <>
+            <Header/>
+            <Marquee/>
+            <Slider/>
+        </>
+    );
 }
 
 export default App;
--- src/header/_header.js
+++ src/header/_header.js
@@ -1,10 +1,29 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import './_header.scss';
+import logo from '../images/fxlogo.png';
 
 class Header extends Component {
-   render() {
-       return <div className="header"><h1>my site</h1></div>
-   }
+    render() {
+        return (
+
+            <div className="header">
+                <div className="main-container">
+
+                    <div className="logo">
+                        <img src={logo} alt="logo"/>
+                    </div>
+                    <nav>
+                        <ul className="navigation">
+                            <li><a href="#">o nas</a></li>
+                            <li><a href="#">handluj na cfd</a></li>
+                            <li><a href="#">handluj na forex</a></li>
+                            <li><a href="#">informacje</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Header;
