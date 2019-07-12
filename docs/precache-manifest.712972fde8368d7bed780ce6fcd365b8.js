22f939cd4d6f291e1b47da8a8666970294bb8332
Lukasz Gawrys <lukaszgawrys13@gmail.com>
2019-07-12 10:06:59 +0200

    fx site proj

---
+++ docs/precache-manifest.712972fde8368d7bed780ce6fcd365b8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a25d4157e0df30d386c9d8685e9c9b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d31d11a797b09df8862",
+    "url": "./static/css/main.a819479b.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "./static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "4d31d11a797b09df8862",
+    "url": "./static/js/main.341a5e97.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "be8c1a3941a46d6a90f55211e8d77239",
+    "url": "./static/media/8bit16-webfont.be8c1a39.woff2"
+  },
+  {
+    "revision": "dfc209a1ed53f74b62b539e7128ee4eb",
+    "url": "./static/media/8bit16-webfont.dfc209a1.woff"
+  },
+  {
+    "revision": "c0fb605aa119cfd32fea790218644c8f",
+    "url": "./static/media/backgroundwallstreet.c0fb605a.jpg"
+  },
+  {
+    "revision": "335961556cd64ad73d781845e0169333",
+    "url": "./static/media/fxlogo1.33596155.png"
+  },
+  {
+    "revision": "428b7928a914fe381f0e351107d9babb",
+    "url": "./static/media/photo3.428b7928.jpeg"
+  }
+]);
--- src/infos/_infos.js
+++ src/infos/_infos.js
@@ -3,35 +3,80 @@ import './_infos.scss';
 
 
 class Infos extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://stocknewsapi.com/api/v1/category?section=general&items=50&token=k32g5v2dbyhepjle0pwolylsj0telifldlnyvtrs')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json.data
+                })
+            })
+    }
 
     render() {
-        return (
-            <div className="infos">
-                <div className="main-container">
-                    <div className="info-section">
-                        <div className="info-img">
-                        </div>
-                        <div className="info-header-and-text">
-                            <div className="info-head">
+        var {isLoaded, items} = this.state;
+
+        if (!isLoaded) {
+            return <div>loading...</div>
+        } else {
+            return (
+                <div>
+                    <div className="infos">
+                        <h3 className="infos-head">
+                            informacje ze świata
+                        </h3>
+                        <div className="main-container">
+                            <div className="info-section">
+                                <div className="info-img">
+                                    <img src={items[1].image_url}/>
+                                </div>
+                                <div className="info-header-and-text">
+                                    <div className="info-head">
+                                        {console.log(items)}
+                                        {items[1].title}
+                                    </div>
+                                    <div className="info-text">
+                                        {items[1].text}
+                                    </div>
+                                    <div className='btn'>
+                                        <a href={items[1].news_url}>
+                                            <button>czytaj więcej</button>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="info-text">
-                            </div>
-                        </div>
-                    </div>
-                    <div className="info-section">
-                        <div className="info-img">
-                        </div>
-                        <div className="info-header-and-text">
-                            <div className="info-head">
-                            </div>
-                            <div className="info-text">
+                            <div className="info-section">
+                                <div className="info-img">
+                                    <img src={items[3].image_url}/>
+                                </div>
+                                <div className="info-header-and-text">
+                                    <div className="info-head">
+                                        {items[3].title}
+                                    </div>
+                                    <div className="info-text">
+                                        {items[3].text}
+                                    </div>
+                                    <div className='btn'>
+                                        <a href={items[3].news_url}>
+                                            <button>czytaj więcej</button>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        )
-
+            )
+        }
     }
 }
 
